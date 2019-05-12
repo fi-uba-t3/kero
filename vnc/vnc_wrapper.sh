256badaec4f6a2a950aa5355204631c052f2da2a
Juan Manuel Fresia <juanmanuelfresia@gmail.com>
2019-05-12 17:16:12 -0300

    Make VNC container start as specific user
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>

--- vnc/vnc_wrapper.sh
+++ vnc/vnc_wrapper.sh
@@ -6,5 +6,13 @@ echo "Starting nslcd daemon"
 sudo nslcd -d > /var/log/nslcd.log 2>&1 &
 echo "nslcd running on $!"
 
+## Wait for nslcd to start
+sleep 10
+
+## Set up entry in /etc/passwd to avoid appearing as default user
+VNC_USER_ID=$(id -u ${VNC_USER})
+VNC_USER_GROUPID=$(id -g ${VNC_USER})
+echo "${VNC_USER}:x:${VNC_USER_ID}:${VNC_USER_GROUPID}:${VNC_USER}:/home/users/${VNC_USER}:/bin/bash" >> /etc/passwd
+
 echo "Running vnc_startup"
-/dockerstartup/vnc_startup.sh "$@"
+su -m ${VNC_USER} /dockerstartup/vnc_startup.sh "$@"
