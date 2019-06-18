9ff2888a401655a55200bf17631303139db3b7a5
Juan Manuel Fresia <juanmanuelfresia@gmail.com>
2019-06-17 23:06:15 -0300

    Modified privision
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>

--- provision.sh
+++ provision.sh
@@ -33,7 +33,7 @@ if [[ "${NODE_ROLE}" == "master" ]]; then
 
         # Leave instructions to other masters and nodes on how to join the cluster.
         cat /vagrant/cache/kubeadm-init.log | grep "experimental-control" -B2 > /vagrant/cache/join-master.sh
-        kubeadm token create --ttl 0s --print-join-command > /vagrant/cache/join.sh
+        tail -2 /vagrant/cache/kubeadm-init.log > /vagrant/cache/join.sh
 
     fi
 
@@ -49,8 +49,9 @@ if [[ "${NODE_ROLE}" == "slave" ]]; then
     echo "$(cat /vagrant/cache/join.sh) --apiserver-advertise-address=${NODE_IP}" | sudo bash -s
 fi
 
-/vagrant/scripts/create-bricks.sh
 # Changing kubelet nodeIP to properly show on kubectl get nodes
 echo "KUBELET_EXTRA_ARGS=--node-ip ${NODE_IP}" >> /var/lib/kubelet/kubeadm-flags.env
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
+
+/vagrant/scripts/create-bricks.sh
