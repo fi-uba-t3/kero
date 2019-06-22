8daf3711ccb00030137d00351d5f486e21a0f8fd
Juan Manuel Fresia <juanmanuelfresia@gmail.com>
2019-06-22 11:14:02 -0300

    Added mountpoint to fstab
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>

--- scripts/create-bricks.sh
+++ scripts/create-bricks.sh
@@ -5,3 +5,5 @@ set -x
 mkdir -p /etc/kubernetes/bricks
 mkfs.ext4 /dev/sdc
 mount /dev/sdc /etc/kubernetes/bricks
+
+echo "/dev/sdc    /etc/kubernetes/bricks    ext4    defaults    0    0" | sudo tee -a /etc/fstab
