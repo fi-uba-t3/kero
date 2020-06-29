3ba302fe6c9a2488de9052fce2f2779edec4bf32
Kajachuan <kevincajachuan@hotmail.com>
2020-06-28 21:49:32 -0300

    Added wiki config files

---
+++ services/webserver/wiki/conf/local.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Dokuwiki's Main Configuration File - Local Settings
+ */
+$conf['title'] = 'KeroWiki';
+$conf['license'] = 'cc-by-sa';
+$conf['useacl'] = 1;
+$conf['authtype'] = 'authldap';
+$conf['superuser'] = '@admin';
+$conf['disableactions'] = 'register';
+$conf['plugin']['authldap']['server'] = 'ldap://10.96.100.100:389';
+$conf['plugin']['authldap']['binddn'] = 'cn=admin,dc=fiuba,dc=com';
+$conf['plugin']['authldap']['bindpw'] = 'admin';
+$conf['plugin']['authldap']['version'] = 3;
+$conf['plugin']['authldap']['usertree'] = 'ou=People,dc=fiuba,dc=com';
+$conf['plugin']['authldap']['userfilter'] = '(&(uid=%{user})(objectClass=posixAccount))';
+$conf['plugin']['authldap']['attributes'] = array('cn', 'uid', 'mail');
---
+++ services/webserver/wiki/conf/users.auth.php
@@ -0,0 +1,10 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+admin:$1$gQUpYItj$Xu1niWbmVKyT76THI.daR1:admin:admin@fiuba.com:admin,user
