bd0d18c248ef73e4977075f4a053a1f002f24886
Ana Czarnitzki <ana.czarnitzki@wildlifestudios.com>
2020-08-02 13:18:23 -0300

    No more hardcoded groupip

--- services/vnc/vnc_wrapper.sh
+++ services/vnc/vnc_wrapper.sh
@@ -14,11 +14,13 @@ VNC_USER_ID=$(id -u ${VNC_USER})
 VNC_USER_GROUPID=$(id -g ${VNC_USER})
 echo "${VNC_USER}:x:${VNC_USER_ID}:${VNC_USER_GROUPID}:${VNC_USER}:/home/users/${VNC_USER}:/bin/bash" >> /etc/passwd
 
+echo "${VNC_GROUP}:x:${VNC_GROUPID}:${VNC_USER}" >> /etc/groups
+
 ## Set up permissions for shared, group and home
 su -m ${VNC_USER} -c "date"
 chown ${VNC_USER_ID} /home/users/${VNC_USER}
 chgrp 500 /mnt/shared
-chgrp 500 /home/users/${VNC_GROUP}
+chgrp ${VNC_GROUPID} /home/users/${VNC_GROUP}
 
 echo "Running vnc_startup"
 su -m ${VNC_USER} /dockerstartup/vnc_startup.sh "$@"
